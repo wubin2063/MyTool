eef8fbadd3b398dfcedb18723b4271070ca21aec
wubin <281532648@qq.com>
2023-08-26 11:51:14 +0800

    TimerTool:添加文件

---
+++ TimeTool/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "TimerTool.h"
+
+int main()
+{
+    TimerTool timer;
+
+    // 测量程序执行时间
+    timer.StartMeasure();
+    // 执行需要测量时间的代码段
+    timer.DelayMicroseconds(100e3); // 模拟耗时代码
+    double elapsed_time = timer.StopMeasure("");
+
+    // 延时功能
+    timer.DelayMicroseconds(500);
+
+    return 0;
+}

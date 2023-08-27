49a54f6b60d26bff7a805f91026cc7cff28576f1
wubin <281532648@qq.com>
2023-08-27 20:49:58 +0800

    更新TimerTool

--- TimeTool/main.cpp
+++ TimeTool/main.cpp
@@ -8,11 +8,11 @@ int main()
     // 测量程序执行时间
     timer.StartMeasure();
     // 执行需要测量时间的代码段
-    timer.DelayMicroseconds(100e3); // 模拟耗时代码
+    TimerTool::DelayMicroseconds(100e3); // 模拟耗时代码
     double elapsed_time = timer.StopMeasure("");
 
     // 延时功能
-    timer.DelayMicroseconds(500);
+    TimerTool::DelayMicroseconds(500);
 
     return 0;
 }
